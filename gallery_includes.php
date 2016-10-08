5ddc209947654ad489225861ff4e66e5caebe223
Ronan Cranley <ronan@gigapansystems.com>
2016-10-07 21:16:49 -0700

    Make the Gallery includes a common file

---
+++ gallery_includes.php
@@ -0,0 +1,10 @@
+
+		<script src="../scripts/jquery.min.js" type="text/javascript"></script>
+		<script src="../scripts/jquery.hoverIntent.min.js" type="text/javascript"></script>
+		<script src="../scripts/jquery.easing.1.3.js" type="text/javascript"></script>
+		<script src="../scripts/jquery.touchSwipe.js" type="text/javascript"></script>
+		<script src="../scripts/jquery.smartresize.js" type="text/javascript"></script>
+		<script src="../scripts/liquid.js" type="text/javascript"></script>
+		<script src="../scripts/slideshows.js" type="text/javascript"></script>
+
+		<script src="https://viewer.marmoset.co/main/marmoset.js"></script>
