3a80564498d3a233b624df4cf47f4bb821220e4f
Ronan Cranley <ronan@gigapansystems.com>
2016-10-22 19:55:03 -0700

    Add new project galleries

---
+++ projects/legends.php
@@ -0,0 +1,32 @@
+<?php
+	// Set the PHP variables to get the images
+	$project = "legends";
+	$title = "legends";
+
+	include('../dir_reader.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Liquid Development :: Project :: <?php echo $title;?></title>
+		<link href="../styles/main.css" rel="stylesheet">
+		<link href="../styles/<?php echo $project;?>.css" rel="stylesheet">
+	</head>
+	<body id="index">
+		<div id="container">
+<?php
+	include('../header.php');
+?>
+			<div id="contents" >
+				<?php
+					include('../gallery.php');
+				?>
+			</div>
+		</div>
+<?php
+	include('../footer.php');
+	include('../gallery_includes.php');
+?>
+	</body>
+</html>
